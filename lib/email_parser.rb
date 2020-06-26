1e274eab4cba75e62869b5cdd7f24205835a55df
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-26 18:20:16 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,8 +11,6 @@ attr_accessor :emails
   end
 
   def parse
-    email_array = @email.split(/[, ]/).uniq
-    email_array.reject! {|element| element.empty?}
-    email_array
+    @emails = @emails.
   end
 end
